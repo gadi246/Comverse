76c2bf4a3f5ae62310a8b02ca841017afe401722
Noa Fisher <Noa Fisher>
2016-06-26 20:32:39 +0300

    onload bulet

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var buls = document.querySelectorAll('.bul') ;
-
+buls[0].style.background = '#2894c9';
 document.body.addEventListener("click", function() {
     for(var i = 0;i < buls.length;i++)  {
         buls[i].style.background = '#fff'
@@ -7,3 +7,4 @@ document.body.addEventListener("click", function() {
     if (event.target.className == "bul")
         event.target.style.background = '#2894c9'
 },true);
+console.log('ok');
